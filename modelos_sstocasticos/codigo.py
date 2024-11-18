a761ed5d268c959e59ff20a4b24171595f15c555
rodrigo <rodrigojzonzin@gmail.com>
2024-11-18 10:42:17 -0300

    modelos estocasticos

---
+++ modelos_sstocasticos/codigo.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random 
+import matplotlib.pyplot as plt
+
+
+
+for k in range(5):
+    y = 30
+    yresult = []
+
+    for t in range(50): 
+        p = random.random()
+
+        if p <= 0.5:
+            y += 1
+        else: 
+            y -= 1
+        
+        yresult.append(y)
+
+
+    yresult = np.array(yresult)
+
+    plt.scatter(y = yresult, x = range(50))
+    plt.ylim([20, 50])
+    plt.savefig(f'teste_{k}.png')
+    plt.plot()
